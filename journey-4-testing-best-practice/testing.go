861da020a189d9c65a27de4e3ca7202b43a050e9
imammaulanaa <32539766+imammaulanaa@users.noreply.github.com>
2025-05-14 08:51:35 +0700

    Menambahkan contoh kode untuk Journey 4: Testing & Best Practices (Unit Testing)

---
+++ journey-4-testing-best-practice/testing.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func TestAdd(t *testing.T) {
+	result := Add(2, 3)
+	if result != 5 {
+		t.Errorf("Add(2, 3) = %d; want 5", result)
+	}
+}
